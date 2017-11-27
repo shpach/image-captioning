c0c93c7ed19e939f315c0bee0518c595e13fc8aa
Dereck <cluo5@illinois.edu>
2017-11-26 21:37:01 -0600

    data pre-process 1.0

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
-from model import ImageCaptioner
+# from model import ImageCaptioner
 import argparse
+import dataset
 
 ''' Retrieve all necessary parameters to train/test the ImageCaptioner '''
 def get_parameters():
@@ -10,110 +11,54 @@ def get_parameters():
     ##########################################
     ### Location of training/testing files ###
     ##########################################
-    parser.add_argument(
-        '--train_img_dir',
-        help='Path to training images',
-        required=True
-    )
-    parser.add_argument(
-        '--train_captions',
-        help='Path to training captions file',
-        required=True
-    )
-    parser.add_argument(
-        '--test_img_dir',
-        help='Path to test images',
-        required=True
-    )
-    parser.add_argument(
-        '--test_captions',
-        help='Path to testing captions file',
-        required=True
-    )
+    parser.add_argument('--train_img_dir', help='Path to training images', default='./data')
+    parser.add_argument('--train_captions', help='Path to training captions file', default='./data')
+    parser.add_argument('--test_img_dir', help='Path to test images', default='./data')
+    parser.add_argument('--test_captions', help='Path to testing captions file', default='./data')
 
     ##########################
     ### Saving checkpoints ###
     ##########################
-    parser.add_argument(
-        '--ckpt_dir',
-        help='Saves checkpoints here',
-        default='./checkpoints/'
-    )
-    parser.add_argument(
-        '--ckpt_freq',
-        help='Save model after this many iterations',
-        default=1000,
-        type=int
-    )
+    parser.add_argument('--ckpt_dir', help='Saves checkpoints here', default='./checkpoints/')
+    parser.add_argument('--ckpt_freq', help='Save model after this many iterations', default=1000, type=int)
 
     ############################
     ### General model params ###
     ############################
-    parser.add_argument(
-        '--solver',
-        help='Optimizer to use: {adam, sgd}',
-        default='adam'
-    )
-    parser.add_argument(
-        '--learning_rate',
-        help='Learning rate',
-        default=1e-3,
-        type=float
-    )
-    parser.add_argument(
-        '--batch_size',
-        help='Specify how many images to use for each iteration',
-        default=20,
-        type=int
-    )
-    parser.add_argument(
-        '--num_epochs',
-        help='Specify how many epochs to run for',
-        default=20,
-        type=int
-    )
+    parser.add_argument('--solver', help='Optimizer to use: {adam, sgd}', default='adam')
+    parser.add_argument('--learning_rate', help='Learning rate', default=1e-3, type=float)
+    parser.add_argument('--batch_size', help='Specify how many images to use for each iteration', default=20, type=int)
+    parser.add_argument('--num_epochs', help='Specify how many epochs to run for', default=20, type=int)
 
     ########################
     ### CNN model params ###
     ########################
-    parser.add_argument(
-        '--cnn_model',
-        help='Type of CNN to use: {custom, vgg16}',
-        default='vgg16'
-    )
-    parser.add_argument(
-        '--cnn_model_file',
-        help='Path to pretrained model file',
-        required=True
-    )
-    parser.add_argument(
-        '--train_cnn',
-        help='Train CNN jointly with the RNN if flag is set',
-        action='store_true'
-        default=False
-    )
+    parser.add_argument('--cnn_model', help='Type of CNN to use: {custom, vgg16}', default='vgg16')
+    parser.add_argument('--cnn_model_file', help='Path to pretrained model file', default='./data')
+    parser.add_argument('--train_cnn', help='Train CNN jointly with the RNN if flag is set', action='store_true', default=False)
 
     ########################
     ### RNN model params ###
     ########################
-    parser.add_argument(
-        '--hidden_size',
-        help='Number of LSTM units to use',
-        default=750,
-        type=int
-    )
-    parser.add_argument(
-        '--dim_embed',
-        help='Dimension for the word embedding',
-        default=300,
-        type=int
-    )
+    parser.add_argument('--hidden_size', help='Number of LSTM units to use', default=750, type=int)
+    parser.add_argument('--dim_embed', help='Dimension for the word embedding', default=300, type=int)
     # parser.add_argument(
     #     '--dim_decoder',
     #     help='Dimension of the vector used for word generation',
     #     default=1000,
     #     type=int
     # )
+
+    ####################################
+    ### Dataset preprocessing params ###
+    ####################################
+    parser.add_argument('--word_table_file', help='File to store the word table', default='./save/word_table.pickle')
+    parser.add_argument('--dataset_file', help='File to store the data set', default='./save/dataset.pickle')
+    parser.add_argument('--glove_file_path', help='Directory containing the glove file', default='./data/glove/')
+    parser.add_argument('--vector_dim', help='word2vec vector dimension', default=50, type=int)
+    parser.add_argument('--data_file_path', help='directory for data', default='./data/Flickr/')
+    parser.add_argument('--image_width', help='resize image width', default = 500)
+    parser.add_argument('--image_height', help='resize image height', default=500)
 
     args = parser.parse_args()
     return args
@@ -122,9 +67,14 @@ def get_parameters():
 def main(_):
 
     # Get dataset.
-    data = ???
+    # data = ???
+    train_data = 0
+    test_data = 0
 
     config = get_parameters()
+
+    #prepare data
+    word_table, data = dataset.prepare_data(config)
 
     # Build model.
     model = ImageCaptioner(config)
--- wordtable.py
+++ wordtable.py
@@ -3,32 +3,47 @@ import pickle as pickle
 import numpy as np
 
 class WordTable():
-    def __init__(self):
+    def __init__(self, vector_dim, save_file):
         self.word2vec = {}
+        self.sentence_dictionary = {}
         self.num_words = 0
         self.word_freq = []
         self.max_num_words = 0
         self.max_sentence_len = 0
+        self.dim_embed = vector_dim
+        self.save_file = save_file
 
-    def build(self, sentences):
-        word_count = {}
-        for sent in sentences:
-                for w in sent.lower().split(' '):
-                    word_count[w] = word_count.get(w, 0) + 1
-                    if w not in self.word2vec:
-                        self.word2vec[w] = 0.01
+    def build(self, dir):
+        # word_count = {}
+        # for sent in sentences:
+        #         for w in sent.lower().split(' '):
+        #             word_count[w] = word_count.get(w, 0) + 1
+        #             if w not in self.word2vec:
+        #                 self.word2vec[w] = 0.01
+        token_file = os.path.join(dir, 'Flickr8k_text/Flickr8k.token.txt')
+        with open(token_file) as f:
+            for line in f:
+                l = line.strip()
+                sentence_split = l.split('\t', 1)
+                self.sentence_dictionary[sentence_split[0]] = sentence_split[1]
+
+
+
 
     def load_gloves(self, dir):
         """ Using GloVe data for word embedding"""
         self.word2vec = {}
         glove_file = os.path.join(dir, 'glove.6B.'+str(self.dim_embed)+'d.txt')
-        with open(glove_file) as f:
+        with open(glove_file, encoding="utf8") as f:
             for line in f:
                 l = line.split()
-                self.word2vec[l[0]] = [float(x)*0.05 for x in l[1:]]
+                self.word2vec[l[0]] = [float(x) for x in l[1:]]
 
     def save(self):
         """ Save the word table to pickle """
+        pickle.dump([self.word2vec, self.sentence_dictionary, self.word_freq, self.num_words], open(self.save_file, 'wb'))
 
     def load(self):
         """ Load the word table from pickle """
+        self.word2vec, self.sentence_dictionary, self.word_freq, self.num_words = pickle.load(open(self.save_file, 'rb'))
+
