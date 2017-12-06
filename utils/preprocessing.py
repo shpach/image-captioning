80bb80bbf37f4c441fdca9fe05d14b466696a81c
Shawn <shpach@github.com>
2017-12-05 22:40:52 -0600

    testing (possibly) done

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -4,3 +4,12 @@ def load_image(path, shape):
     img = imread(path, mode='RGB')
     img = imresize(img, shape)
     return img
+
+def resize_images(data, shape):
+	for train_idx in range(len(data.training_data)):
+		train_img = data.training_data[train_idx]
+		data.training_data[train_idx] = imresize(train_img, shape)
+
+	for test_idx in range(len(data.validation_data)):
+		test_img = data.validation_data[test_idx]
+		data.validation_data[test_idx] = imresize(test_img, shape)
